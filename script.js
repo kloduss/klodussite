2ab8a2cb0db839f9ad32c92db28d7fed5f1d8ce4
kloduss <73596426+kloduss@users.noreply.github.com>
2026-02-16 16:03:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+//Ховати хедер при скролі
+if (window.innerWidth > 768) {
+    let lastScrollTop = 0;
+    let header = document.querySelector('header');
+    let scrollThreshold = 50;
+
+    window.addEventListener('scroll', function() {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        
+        if (scrollTop > scrollThreshold) {
+            if (scrollTop > lastScrollTop) {
+                header.classList.add('header-hidden');
+            } else {
+                header.classList.remove('header-hidden');
+            }
+        } else {
+            header.classList.remove('header-hidden');
+        }
+        
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+    }, false);
+}
+
+// Кнопка вгору
+const backToTopBtn = document.getElementById('backToTop');
+
+window.addEventListener('scroll', function() {
+    if (window.pageYOffset > 300) {
+        backToTopBtn.classList.add('show');
+    } else {
+        backToTopBtn.classList.remove('show');
+    }
+});
+
+backToTopBtn.addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
